af96622142c020eb7aeddd4be57af72cfba57132
Simon Phillips <simon.phillips01@icloud.com>
2019-03-21 11:41:11 +0000

    Created an animal class and animal objects

---
+++ src/Animals/Cat.java
@@ -0,0 +1,47 @@
+package Animals;
+
+public class Cat {
+    private String name;
+    private int age;
+    private String breed;
+    private String size;
+
+    public Cat(String name, int age, String breed, String size){
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.size = size;
+    }
+
+    public String meow (String size) {
+        String meow = "'";
+        if(size.equalsIgnoreCase("small")){
+            meow = "silent meow";
+        }
+        else if(size.equalsIgnoreCase("medium")){
+            meow = "meow";
+        }
+        else if(size.equalsIgnoreCase("large")){
+            meow = "MEOW!";
+        }
+        return meow;
+    }
+
+    public String sleep (String breed) {
+        String sleep = "";
+        if(breed.equalsIgnoreCase("munchkin")){
+            sleep = "You are too small therefore I sleep all day";
+        }
+        else if(breed.equalsIgnoreCase("Egyptian Mau")){
+            sleep = "I'm too sophisticated to sleep";
+        }
+        else if(breed.equalsIgnoreCase("Ragdoll")){
+            sleep = "I can never sleep because I like playinng too much";
+        }
+        return sleep;
+    }
+
+    public String beHuman(String name, int age, String breed) {
+        return "Hello human, I'm " + name + " and I am a " + age + " year old " + breed + ", What breed are you?";
+    }
+}
---
+++ src/Animals/createAnimal.java
@@ -0,0 +1,64 @@
+package Animals;
+
+import com.sun.source.util.SourcePositions;
+
+import java.util.Scanner;
+
+public class createAnimal {
+    Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        createAnimal create = new createAnimal();
+        create.getAnimalType();
+    }
+
+    private void getAnimalType() {
+        System.out.println("What animal would you like to create?");
+        String animal = myScanner.nextLine();
+        if(animal.equalsIgnoreCase("dog")){
+            createDog();
+        } else if(animal.equalsIgnoreCase("cat")){
+            createCat();
+        } else {
+            System.out.println(animal + " is not an animal we can currently create, please try again");
+        }
+    }
+
+    private void createDog() {
+        System.out.println("What is the dogs name?");
+        String name = myScanner.nextLine();
+        System.out.println("How big is the dog?");
+        String size = myScanner.nextLine();
+        System.out.println("How old is the dog?");
+        int age = Integer.parseInt(myScanner.nextLine());
+        System.out.println("What breed is the dog?");
+        String breed = myScanner.nextLine();
+        dog myDog = new dog(name, size, age, breed);
+
+        System.out.println(myDog.bark(size));
+        System.out.println(myDog.play(breed));
+        System.out.println(myDog.beHuman(name, age, breed));
+
+        getAnimalType();
+
+    }
+
+    private void createCat() {
+        System.out.println("What is the cats name?");
+        String name = myScanner.nextLine();
+        System.out.println("How big is your cat?");
+        String size = myScanner.nextLine();
+        System.out.println("How old is the cat?");
+        int age = Integer.parseInt(myScanner.nextLine());
+        System.out.println("What breed is the cat?");
+        String breed = myScanner.nextLine();
+        Cat myCat = new Cat(name, age, size, breed);
+
+        System.out.println(myCat.sleep(breed));
+        System.out.println(myCat.meow(size));
+
+        getAnimalType();
+
+    }
+
+}
